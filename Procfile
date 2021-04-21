efb03b5dd3c05ab5b599439254b67397052a6b3d
ghettoDdOS <pen.egor2002@gmail.com>
2021-04-21 22:16:34 +0300

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn test_rsue1.wsgi --log-file -
+web: gunicorn test_rsue1.wsgi
--- test_rsue1/settings.py
+++ test_rsue1/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
